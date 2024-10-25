1c78f9feb9a1f3431eee8161a6fc812fb7c5dc48
Gema Sanchez Mu?oz <GSANMUO@upv.edu.es>
2024-10-25 18:05:18 +0200

    actualizar gema

--- FrontEndExpo/App.js
+++ FrontEndExpo/App.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from 'react-native-vector-icons';
 import { createStackNavigator } from "@react-navigation/stack";
 import CrearEvento from "./interfaz/CrearEvento";
 import VerEvento from "./interfaz/VerEvento";
+import perfil from "./interfaz/perfil";
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
 
 export default function App() {
   const [eventos, setEventos] = useState([]);
@@ -27,22 +32,41 @@ export default function App() {
 
   return (
     <NavigationContainer>
-       <Stack.Navigator initialRouteName="VerEvento">
-        {/* Pantalla VerEvento */}
-        <Stack.Screen 
-          name="VerEvento" 
-          options={{ title: 'Mis eventos' }}
-        >
-          {props => <VerEvento {...props} eventos={eventos} />}
-        </Stack.Screen> 
-        
-        {/* Pantalla CrearEvento */}
-        <Stack.Screen 
-          name="CrearEvento" 
-          component={CrearEvento} 
-          options={{ title: 'Modificar evento' }} 
-        />
-      </Stack.Navigator>
+      <Tab.Navigator
+        screenOption={{
+          tabBarActiveintColor: 'tomato',
+          tabBarInactiveintColor: 'gray',
+        }}>
+        {/* Stack para la pantalla VerEvento */}
+        <Tab.Screen name="VerEventos" options={{ title: 'Mis Eventos', tabBarIcon:({color, size}) => (
+            <Ionicons name = "list" color={color} size={size} /> ), }}>
+          {props => (
+            <Stack.Navigator>
+              <Stack.Screen 
+                name="VerEvento" 
+                options={{ headerShown: false }} // Oculta el header de esta pantalla
+              >
+                {props => <VerEvento {...props} eventos={eventos} />}
+              </Stack.Screen>
+              <Stack.Screen 
+                name="CrearEvento" 
+                component={CrearEvento} 
+                options={{ title: 'Modificar Evento' }} 
+              />
+            </Stack.Navigator>
+          )}
+        </Tab.Screen>
+
+        {/* Otra pestaña para la creación de eventos */}
+        <Tab.Screen 
+          name="perfil" component={perfil} options={{ 
+            title: 'tu perfil',
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="person" color={color} size={size} />
+              ),
+            }} 
+            />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
