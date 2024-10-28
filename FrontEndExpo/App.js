068424afba5dbdac008062e1073ebb174d7b7948
Gema Sanchez Mu?oz <GSANMUO@upv.edu.es>
2024-10-28 18:32:01 +0100

    uso esto javi

--- FrontEndExpo/App.js
+++ FrontEndExpo/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { View, ActivityIndicator, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from 'react-native-vector-icons';
@@ -6,10 +7,37 @@ import { createStackNavigator } from "@react-navigation/stack";
 import CrearEvento from "./interfaz/CrearEvento";
 import VerEvento from "./interfaz/VerEvento";
 import perfil from "./interfaz/perfil";
+import Archivos from "./interfaz/Archivos";
+import mapa from "./interfaz/mapa";
+import notificaciones from "./interfaz/notificaciones";
+import DetalleEvento from "./interfaz/DetalleEvento";
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
+function VerEventosStack({ eventos }) {
+  return(
+    <Stack.Navigator>
+      <Stack.Screen 
+        name="VerEvento" 
+        options={{ headerShown: false }} // Oculta el header de esta pantalla
+      >
+        {props => <VerEvento {...props} eventos={eventos} />}
+      </Stack.Screen>
+      <Stack.Screen 
+        name="CrearEvento" 
+        component={CrearEvento} 
+        options={{ title: 'Modificar Evento' }} 
+      />
+      <Stack.Screen 
+        name="DetalleEvento" 
+        component={DetalleEvento} 
+        options={{ title: 'Detalle del Evento' }} 
+      />
+      
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
   const [eventos, setEventos] = useState([]);
@@ -33,30 +61,42 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator
-        screenOption={{
-          tabBarActiveintColor: 'tomato',
-          tabBarInactiveintColor: 'gray',
+        screenOptions={{
+          tabBarActiveTintColor: 'tomato',
+          tabBarTinactiveintColor: 'gray',
         }}>
         {/* Stack para la pantalla VerEvento */}
-        <Tab.Screen name="VerEventos" options={{ title: 'Mis Eventos', tabBarIcon:({color, size}) => (
-            <Ionicons name = "list" color={color} size={size} /> ), }}>
-          {props => (
-            <Stack.Navigator>
-              <Stack.Screen 
-                name="VerEvento" 
-                options={{ headerShown: false }} // Oculta el header de esta pantalla
-              >
-                {props => <VerEvento {...props} eventos={eventos} />}
-              </Stack.Screen>
-              <Stack.Screen 
-                name="CrearEvento" 
-                component={CrearEvento} 
-                options={{ title: 'Modificar Evento' }} 
-              />
-            </Stack.Navigator>
-          )}
+        <Tab.Screen 
+          name="VerEventos" options={{ title: 'Mis Eventos', tabBarIcon:({color, size}) => (
+            <Ionicons name = "list" color={color} size={size} /> ), }}
+        >
+          {() => <VerEventosStack eventos={eventos} />}
         </Tab.Screen>
-
+        <Tab.Screen 
+          name="Archivos" component={Archivos} options={{ 
+            title: 'Tus archivos',
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="folder" color={color} size={size} />
+              ),
+            }} 
+          />
+        <Tab.Screen 
+          name="Mapa" component={mapa} options={{ 
+            title: 'Mapa',
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="map" color={color} size={size} />
+              ),
+            }} 
+          />
+        <Tab.Screen 
+          name="notificaciones" component={notificaciones} options={{ 
+            title: 'Notificaciones',
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="notifications" color={color} size={size} />
+              ),
+            }} 
+          />
+          
         {/* Otra pestaña para la creación de eventos */}
         <Tab.Screen 
           name="perfil" component={perfil} options={{ 
@@ -70,3 +110,10 @@ export default function App() {
     </NavigationContainer>
   );
 }
+const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
