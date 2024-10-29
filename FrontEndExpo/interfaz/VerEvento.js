51b9116d7aaf2cada2ac1824cf0f7732ac24e29e
Maria Victoria Egido Castillo <MVEGICAS@upv.edu.es>
2024-10-29 21:07:27 +0100

    así creo que está cpn lo nuevo mio

--- FrontEndExpo/interfaz/VerEvento.js
+++ FrontEndExpo/interfaz/VerEvento.js
@@ -59,30 +59,6 @@ const VerEvento = ({ eventos, navigation, setEventos, actualizarEventos }) => {
     const { hora, fechaFormateada } = formatearFechaHora(item.fecha);
 
     return (
-<<<<<<< HEAD
-      <View style={styles.eventCard}>
-        <Image
-          source={{ uri: item.foto || 'https://via.placeholder.com/150' }}
-          style={styles.eventImage}
-        />
-        <View style={styles.eventInfo}>
-          <Text style={styles.eventTitle}>{item.nombre}</Text>
-          <View style={styles.infoRow}>
-            <FontAwesome name="clock-o" size={20} color="gray" />
-            <Text style={styles.eventTime}>{hora}</Text>
-          </View>
-
-          <View style={styles.infoRow}>
-            <MaterialIcons name="date-range" size={20} color="gray" />
-            <Text style={styles.eventDate}>{fechaFormateada}</Text>
-          </View>
-
-          <View style={styles.infoRow}>
-            <MaterialIcons name="location-on" size={20} color="gray" />
-            <Text style={styles.eventLocation}>{item.ubicacion}</Text>
-          </View>
-
-=======
       <TouchableOpacity onPress={() => navigation.navigate ('DetalleEvento', { evento: item})}>
         <View style={styles.eventCard}>
           <Image
@@ -114,32 +90,12 @@ const VerEvento = ({ eventos, navigation, setEventos, actualizarEventos }) => {
               <Text style={styles.eventAforo}>{`${item.aforo}/25`}</Text>
             </View>
           </View>
->>>>>>> main
           <View style={styles.infoRow}>
             <FontAwesome name="users" size={20} color="gray" />
             <Text style={styles.eventAforo}>{`0/${item.aforo}`}</Text>
           </View>
         </View>
 
-<<<<<<< HEAD
-        <View style={styles.actions}>
-          <TouchableOpacity
-            style={styles.actionButton}
-            onPress={() => navigation.navigate('CrearEvento', { evento: item })}
-          >
-            <FontAwesome name="refresh" size={24} color="black" />
-            <Text>Modificar</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.actionButton}
-            onPress={() => confirmarEliminar(item.id)}
-          >
-            <MaterialIcons name="delete" size={24} color="black" />
-            <Text>Eliminar</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-=======
           <View style={styles.actions}>
             <TouchableOpacity 
               style={styles.actionButton} 
@@ -154,7 +110,6 @@ const VerEvento = ({ eventos, navigation, setEventos, actualizarEventos }) => {
             </TouchableOpacity>
           </View>
       </TouchableOpacity>
->>>>>>> main
     );
   };
 
